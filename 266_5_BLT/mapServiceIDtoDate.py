29d491bfc37e142bc7cb5264322c295557d61581
olaroos <ola.per.roos@gmail.com>
2014-08-18 17:45:34 +0200

    changed trace-bash functions to have no more than 3 cases, trimming down stop_times.txt is now automatic for each track; changed mapServiceIDtoDate.py to handle dates with 3 service_ids

--- 266_5_BLT/mapServiceIDtoDate.py
+++ 266_5_BLT/mapServiceIDtoDate.py
@@ -43,22 +43,42 @@ for item in arrayA:
 			arrayB[subItem] = []
 			arrayB[subItem].append(item)			
 
+print arrayB
+
 arrayC = {}
 
 for item in arrayB:	
 	if (len(arrayB[item]) > 1):
 		item1 = arrayB[item][0]
-		item2 = arrayB[item][1]
+		item2 = arrayB[item][1]		
 		arrayC[item] = []
-		if (int(matchNames[item1][0][0:2]) > int(matchNames[item2][0][0:2])):			
-			arrayC[item].append(item2)
-			arrayC[item].append(matchNames[item2][0])
-			arrayC[item].append(item1)
-			arrayC[item].append(matchNames[item1][0])
+		if (len(arrayB[item]) == 3):
+			item3 = arrayB[item][2]
+			timeItem1 = int(matchNames[item1][0][0:2])
+			timeItem2 = int(matchNames[item2][0][0:2])
+			timeItem3 = int(matchNames[item3][0][0:2])
+
+			list1 		= {timeItem1 : item1, timeItem2 : item2, timeItem3 : item3}
+			list2 		= [timeItem1, timeItem2, timeItem3]			
+			list2.sort()
+			for i in range(len(list2)):
+				arrayC[item].append(list1[list2[i]])
+				arrayC[item].append(matchNames[list1[list2[i]]][0])
+		else:
+			if (int(matchNames[item1][0][0:2]) > int(matchNames[item2][0][0:2])):		
+				arrayC[item].append(item2)
+				arrayC[item].append(matchNames[item2][0])
+				arrayC[item].append(item1)
+				arrayC[item].append(matchNames[item1][0])
+			else:
+				arrayC[item].append(item1)
+				arrayC[item].append(matchNames[item1][0])
+				arrayC[item].append(item2)
+				arrayC[item].append(matchNames[item2][0])
 	else:
 		arrayC[item] = arrayB[item]
 		
-
+print arrayC
 jsonFile = json.dumps(arrayC, ensure_ascii=True)
 
 theFile = open('dumpPy/266_'+track+'_BLT_service_id_with_datesJSON', 'w')
--- makeAndPushCurrentCSS.py
+++ makeAndPushCurrentCSS.py
@@ -210,15 +210,15 @@ c.execute("""truncate table css""")
 c.execute("""INSERT INTO css (jsonFile) VALUES (%s);""", (output,))
 conn.close()
 
-DB              = 'VBUSS'
-DB_HOST         = '188.226.223.188'
-DB_USER         = 'root'
-DB_PASSWORD     = 'lemmeltagetforti'
-conn = MySQLdb.Connection(db=DB, host=DB_HOST, user=DB_USER,passwd=DB_PASSWORD)
-c = conn.cursor()
+# DB              = 'VBUSS'
+# DB_HOST         = '188.226.223.188'
+# DB_USER         = 'root'
+# DB_PASSWORD     = 'lemmeltagetforti'
+# conn = MySQLdb.Connection(db=DB, host=DB_HOST, user=DB_USER,passwd=DB_PASSWORD)
+# c = conn.cursor()
 
-c.execute("set autocommit = 1")
-c.execute("""truncate table css""")
-c.execute("""INSERT INTO css (jsonFile) VALUES (%s);""", (output,))
-conn.close()
+# c.execute("set autocommit = 1")
+# c.execute("""truncate table css""")
+# c.execute("""INSERT INTO css (jsonFile) VALUES (%s);""", (output,))
+# conn.close()
 
