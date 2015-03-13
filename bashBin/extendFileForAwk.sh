7ec90feeb2e0a9d4fd25c9916c4c63be2cd2c14b
olaroos <ola.per.roos@gmail.com>
2015-03-13 13:54:02 +0100

    completed extendFileForAwk.sh

--- bashBin/extendFileForAwk.sh
+++ bashBin/extendFileForAwk.sh
@@ -1,9 +1,14 @@
-# cat ../calendar.txt | awk 'BEGIN {FS=","} 
-# {
-# 	print $0 
-# }' | echo "," >> tmpCalendar.txt
-rm tmpCalendar.txt
+
 while read line 
 do echo $line"," | tr -d "\n" | tr -d "\r" >> tmpCalendar.txt;
 echo "" >> tmpCalendar.txt
 done < "../calendar.txt"
+mv tmpCalendar.txt ../calendar.txt
+
+# rm tmpCalendar.txt
+# while read line 
+# do echo $line"," | tr -d "\n" | tr -d "\r" >> tmpCalendar.txt;
+# echo "" >> tmpCalendar.txt
+# done < "../calendar_dates.txt"
+# mv tmpCalendar.txt ../calendar_dates.txt
+# rm tmpCalendar.txt
